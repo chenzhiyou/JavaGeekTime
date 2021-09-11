fc151ccb361ba417c9665973a92206838b212124
lixiaochen <lixiaochen@meishubao.com>
2021-09-11 21:52:09 +0800

    再次学习String

---
+++ src/main/java/code021/LearnSting.java
@@ -0,0 +1,20 @@
+package code021;
+
+public class LearnSting {
+    public static void main(String[] args) {
+        String content = "1234567890abcdefg";
+
+        System.out.println(content.length());
+
+        System.out.println(content.toUpperCase());
+
+        System.out.println(content.toLowerCase());
+
+        System.out.println(content);
+
+        System.out.println(content.charAt(1));
+
+        System.out.println(content.substring(5));
+        System.out.println(content.substring(1,5));
+    }
+}
